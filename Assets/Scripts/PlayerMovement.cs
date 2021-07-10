a52b02684bdae57b58630a054946de707b36e2ae
Nathan Anderson <SniperSmiley@gmail.com>
2021-07-10 01:11:12 -0700

    Update PlayerMovement.cs
    Added jumping, it's kind of wonky

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -13,14 +13,18 @@ public class PlayerMovement : MonoBehaviour
 
     public float runSpeed = 40f;
     public float horizontalMove = 0f;
+    bool jump = false;
     // Update is called once per frame
     void Update()
     {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        if (Input.GetButtonDown("Jump"))
+            jump = true;
     }
 
     void FixedUpdate()
     {
-        controller.Move(horizontalMove * Time.fixedDeltaTime, false, false);
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
     }
 }
