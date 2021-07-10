127508a447fad012562082554550080c4a9ce5f9
Jack Eatock <46260379+Jack-Eatock@users.noreply.github.com>
2021-07-10 19:03:01 +0100

    Title Screen
    Added Title, Options and Start screen.

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    // Play background music, manage what clips are being played in general. 
+
+}
