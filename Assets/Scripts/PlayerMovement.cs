5b92e3792dd58a609ed5d488b5da11610f3db375
Nathan Anderson <SniperSmiley@gmail.com>
2021-07-09 21:55:54 -0700

    Added Basic Lab Scene
    The player is a circle, and can move left and right.
    Includes a script from Brackeys

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public float runSpeed = 40f;
+    public float horizontalMove = 0f;
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, false);
+    }
+}
